ae0227f59dcbd458490d3fe45384696171054984
joohyungsongnbcc <joohyungsong0205@gmail.com>
2021-04-30 17:38:44 -0300

    homework1 commit

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //This is the main entry point for a flutter app
+  runApp(
+    //This inflates a widget and attaches it to the screen
+    Center(
+      child: Text('Hello World',
+      textDirection:TextDirection.ltr,
+      ),
+      ),
+  );
+}
